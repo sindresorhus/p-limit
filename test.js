60bcb15c13de6f9bd9bae30f799706f332fc816b
Andy Edwards <jedwards@fastmail.com>
2020-04-21 15:53:15 -0500

    test: fix clearQueue test

--- test.js
+++ test.js
@@ -130,13 +130,14 @@ test('clearQueue', async t => {
 	const limit = pLimit(1);
 
 	Array.from({length: 1}, () => limit(() => delay(1000)));
-	Array.from({length: 3}, () => limit(() => delay(1000)).catch(
-		error => t.is(error.message, 'queue cleared before function was invoked')
-	));
+	const pendingPromises = Array.from({length: 3},
+		() => t.throwsAsync(limit(() => delay(1000)), 'queue cleared before function was invoked')
+	);
 
 	await Promise.resolve();
 	t.is(limit.pendingCount, 3);
 	limit.clearQueue();
+	await Promise.all(pendingPromises);
 	t.is(limit.pendingCount, 0);
 });
 
