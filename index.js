4ab2813001fae4345d02e7fee394222cffd371d9
sethp <seth.pellegrino@gmail.com>
2020-07-12 06:35:19 -0700

    Fix error on invalid concurrency (#35)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const pTry = require('p-try');
 
 const pLimit = concurrency => {
 	if (!((Number.isInteger(concurrency) || concurrency === Infinity) && concurrency > 0)) {
-		return Promise.reject(new TypeError('Expected `concurrency` to be a number from 1 and up'));
+		throw new TypeError('Expected `concurrency` to be a number from 1 and up');
 	}
 
 	const queue = [];
